6a4dd374bdbcacb9a9fff90270d0a91fd02ae86c
Jason McBride <jmcbride.isye@gmail.com>
2026-03-02 04:58:07 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { LayoutDashboard, FileText, TrendingUp, ChevronDown, Menu, X } from 'lucide-react';
